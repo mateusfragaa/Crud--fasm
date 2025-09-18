ca75f81d39e7258500f0c9650ad195d2a4249375
mateus <160082106+mateusfragaa@users.noreply.github.com>
2025-09-18 20:49:07 -0300

    Update index.php
    Modificações navbar, troca de conteúdo de texto

--- view/index.php
+++ view/index.php
@@ -37,7 +37,7 @@
                                     </li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#" id="submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Aldecir Fonseca
+                                            mateusfragaa
                                         </a>
                                         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="submenu">
                                             <li><a class="dropdown-item" href="index.php?action=list">Departamento</a></li>
